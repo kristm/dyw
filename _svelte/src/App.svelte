74460a41679d982e9ef4c12973e5c51596a541eb
Krist Menina <krist@quipper.com>
2021-04-16 22:00:45 +0800

    position bg

--- _svelte/src/App.svelte
+++ _svelte/src/App.svelte
@@ -9,29 +9,40 @@
   main
     h1 hello #{name} :metal:
 
-    - for (var i=0; i<4; i++)
-      div(class='bg bg'+i)
+    div(class='bg')
+      - for (var i=0; i<4; i++)
+        div(class='bg'+i)
 
 </template>
 <style lang="stylus">
 	main
-    height 2000px
+    height 3000px
+    position relative
+    border: 5px solid blue
 
   .bg {
+    border: 5px solid red;
     width: 100%;
-    background-position: bottom center;
-    background-repeat: no-repeat;
+    height: 50%;
+    position: absolute;
+    bottom: 0;
+
+    > div {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      bottom: 0;
+      background-position: bottom center;
+      background-repeat: no-repeat;
+    }
   }
 
-  .bg0 {
-    background-image: url(/images/bg0.png);
-    height: 100%;
-  }
+  //bspace = ( -6000px -4000px -2000px 0 )
 
-  .bg1 {
-    background-image: url(/images/bg1.png);
-    height: 100%;
-  }
+  for num in (0..3)
+    .bg{num}
+      background-image url('/images/bg'+num+'.png')
+      //bottom bspace[num]
 
 	@media (min-width: 640px)
 		main
